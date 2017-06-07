28c5c1bf973e5719167d1c6ad55d130ecc1792e5
leejabba <dlgodrhs@gmail.com>
2017-06-08 02:50:56 +0900

    version 1.2 - 색상, 두께 변환까지 완료 - 뷰에 터치해서 그리기 시작할 때 객체를 생성하게 할 수는 없을지 고민해야 함

---
+++ app/src/main/java/kr/heythisway/drawingnote/Board.java
@@ -0,0 +1,80 @@
+package kr.heythisway.drawingnote;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import static kr.heythisway.drawingnote.MainActivity.BLUE;
+import static kr.heythisway.drawingnote.MainActivity.GREEN;
+import static kr.heythisway.drawingnote.MainActivity.RED;
+
+/**
+ * Created by SMARTHINK_MBL13 on 2017. 6. 8..
+ */
+
+public class Board extends View {
+    Paint paint;
+    Path path;
+
+    public Board(Context context) {
+        super(context);
+    }
+
+    public void setPath(int color, int width) {
+        path = new Path();
+        paint = new Paint();
+        switch (color) {
+            case GREEN:
+                paint.setColor(Color.GREEN);
+                break;
+            case BLUE:
+                paint.setColor(Color.BLUE);
+                break;
+            case RED:
+                paint.setColor(Color.RED);
+                break;
+        }
+        paint.setStrokeWidth(width);
+        paint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //내가 터치한 좌표를 꺼낸다.
+        float x = event.getX();
+        float y = event.getY();
+
+        Log.e("log", "======================= onTouchEvent : x = " + x + ", y = " + y);
+
+        //액션 체크
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x, y);  //그리지 않고 이동
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(x, y);  //바로 이전점과 현재점 사이에 줄을 그어준다.
+                break;
+            case MotionEvent.ACTION_UP:
+                path.lineTo(x, y);
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                Toast.makeText(getContext(), "언제찍히니?", Toast.LENGTH_SHORT).show();
+        }
+
+        invalidate();   //참고로 서브스트림에서는 postInvalidate()를 사용한다.
+
+        return true;
+    }
+}
--- app/src/main/java/kr/heythisway/drawingnote/MainActivity.java
+++ app/src/main/java/kr/heythisway/drawingnote/MainActivity.java
@@ -1,29 +1,20 @@
 package kr.heythisway.drawingnote;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     FrameLayout frameLayout;
     RadioGroup radioGroup;
     SeekBar seekBar;
+    TextView seekProcess;
 
     final static int GREEN = 1;
     final static int BLUE = 2;
@@ -32,132 +23,77 @@ public class MainActivity extends AppCompatActivity {
     int currentColor = 1;
     int currentWidth = 10;
 
-    Board board, board1, board2, board3, boardSample;
-    Paint paint;
-
-    int color;
+    Board board;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final List<Board> boards = new ArrayList<>();
+        //Board 객체에 Paint 속성 설정한 후 프레임 레이아웃 뷰에 추가하기
+        frameLayout = (FrameLayout) findViewById(R.id.layout);
         board = new Board(getBaseContext());
         board.setPath(currentColor, currentWidth);
-//        boards.add(board);
+        frameLayout.addView(board);
 
-        frameLayout = (FrameLayout) findViewById(R.id.layout);
+        // 라디오 버튼 (선의 색상)
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
-
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-                if (group.getId() == R.id.radioGroup) {
-                    switch (checkedId) {
-                        case R.id.radioBtnGreen:
-                            if (currentColor != GREEN) {
-                                currentColor = GREEN;
-                                board = new Board(getBaseContext());
-                                boardSample = board;
-                                boardSample.setPath(currentColor, currentWidth);
-                                Toast.makeText(MainActivity.this, "녹색을 선택하셨습니다.", Toast.LENGTH_SHORT).show();
-                                frameLayout.addView(boardSample);
-                            }
-                            break;
-                        case R.id.radioBtnBlue:
-                            if (currentColor != BLUE) {
-                                currentColor = BLUE;
-                                board = new Board(getBaseContext());
-                                boardSample = board;
-                                boardSample.setPath(currentColor, currentWidth);
-                                Toast.makeText(MainActivity.this, "파랑색을 선택하셨습니다.", Toast.LENGTH_SHORT).show();
-                                frameLayout.addView(boardSample);
-                            }
-                            break;
-                        case R.id.radioBtnRed:
-                            if (currentColor != RED) {
-                                currentColor = RED;
-                                board = new Board(getBaseContext());
-                                boardSample = board;
-                                boardSample.setPath(currentColor, currentWidth);
-                                Toast.makeText(MainActivity.this, "빨강색을 선택하셨습니다.", Toast.LENGTH_SHORT).show();
-                                frameLayout.addView(boardSample);
-                            }
-                            break;
-                    }
-                }
+                selectRadio(group, checkedId);
             }
         });
 
+        // 시크바 (선의 두께)
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekProcess = (TextView) findViewById(R.id.seekProcess);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                currentWidth = progress;
+                seekProcess.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                setPaintProperty(currentColor, currentWidth);
+            }
+        });
+    }
+
+    // 라디오 버튼 선택 분기 메서드
+    private void selectRadio(RadioGroup group, int checkedId) {
+        if (group.getId() == R.id.radioGroup) {
+            switch (checkedId) {
+                case R.id.radioBtnGreen:
+                    setPaintProperty(GREEN, currentWidth);
+                    break;
+                case R.id.radioBtnBlue:
+                    setPaintProperty(BLUE, currentWidth);
+                    break;
+                case R.id.radioBtnRed:
+                    setPaintProperty(RED, currentWidth);
+                    break;
+            }
+        }
+    }
+
+    // 새로운 Board 객체를 생성해서 라디오 버튼 색상의 Paint 속성을 설정 후 프레임 레이아웃 뷰에 추가
+    private void setPaintProperty(int color, int width) {
+        Log.e("seek", "=======================  seek바가 움직임에따라 setPaintProperty가 실행되었습니다.");
+        currentColor = color;
+        currentWidth = width;
+
+        board = new Board(getBaseContext());
+        board.setPath(currentColor, currentWidth);
         frameLayout.addView(board);
-
     }
 
-    class Board extends View {
-        Paint paint;
-        Path path;
-//        List<Path> paths = new ArrayList<>();
 
-
-        public Board(Context context) {
-            super(context);
-        }
-
-        public void setPath(int color, int width) {
-            path = new Path();
-            paint = new Paint();
-            switch (color) {
-                case GREEN:
-                    paint.setColor(Color.GREEN);
-                    break;
-                case BLUE:
-                    paint.setColor(Color.BLUE);
-                    break;
-                case RED:
-                    paint.setColor(Color.RED);
-                    break;
-            }
-            paint.setStrokeWidth(width);
-            paint.setStyle(Paint.Style.STROKE);
-//            paths.add(path);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-//            for (Path path : paths) {
-            canvas.drawPath(path, paint);
-//            }
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            //내가 터치한 좌표를 꺼낸다.
-            float x = event.getX();
-            float y = event.getY();
-
-            Log.e("log", "======================= onTouchEvent : x = " + x + ", y = " + y);
-
-            //액션 체크
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    path.moveTo(x, y);  //그리지 않고 이동
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    path.lineTo(x, y);  //바로 이전점과 현재점 사이에 줄을 그어준다.
-                    break;
-                case MotionEvent.ACTION_UP:
-                    path.lineTo(x, y);
-                    break;
-                case MotionEvent.ACTION_POINTER_UP:
-                    Toast.makeText(getContext(), "언제찍히니?", Toast.LENGTH_SHORT).show();
-            }
-
-            invalidate();   //참고로 서브스트림에서는 postInvalidate()를 사용한다.
-
-            return true;
-        }
-    }
 }
